238892c0c2f1936a542ba223abbe8a3a0e9230c6
AgileRampler <43984000+AgileRampler@users.noreply.github.com>
2026-05-05 04:54:54 +0530

    Update mainPage.jsx

--- src/pages/main/mainPage.jsx
+++ src/pages/main/mainPage.jsx
@@ -213,7 +213,7 @@ const MainPage = () => {
           </div>
         </div>
 
-        {/* About Section — ✅ id only on outer div, removed duplicate */}
+
         <div id='about' className='m-14 rounded-lg shadow-2xl bg-white'>
           <div className='grid grid-cols-2 gap-4'>
 
@@ -310,7 +310,7 @@ const MainPage = () => {
           <div className='text-purple-800 font-bold text-2xl mb-4'>FEATURED PROJECT</div>
           <div className='flex justify-between items-center mb-10'>
             <div className='text-4xl font-bold'>Some Things I've Built</div>
-            <Link to="ViewProject"  className="text-purple-700 font-semibold text-xl hover:underline flex items-center gap-1">
+            <Link onClick={() => window.scrollTo(0, 0)} to="/ViewProject"  className="text-purple-700 font-semibold text-xl hover:underline flex items-center gap-1">
               View all projects →
             </Link>
           </div>
